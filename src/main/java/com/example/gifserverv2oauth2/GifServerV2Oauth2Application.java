48992679f7d01ef2ffecc6653de8647f6811005c
kimjihoon3106 <s24021@gsm.hs.kr>
2024-09-03 19:53:55 +0900

    init

---
+++ src/main/java/com/example/gifserverv2oauth2/GifServerV2Oauth2Application.java
@@ -0,0 +1,13 @@
+package com.example.gifserverv2oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifServerV2Oauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GifServerV2Oauth2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gifserverv2oauth2/GifServerV2Oauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gifserverv2oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GifServerV2Oauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
